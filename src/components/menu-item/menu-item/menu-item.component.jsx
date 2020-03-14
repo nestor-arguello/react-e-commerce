779af06df61a186cb8a705f0cdd68d058689d9d8
Nestor Arguello <nestor.arguello@gmail.com>
2020-03-14 11:51:56 -0400

    add simple navigation to menu-items

--- src/components/menu-item/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match, ...props }) => (
   <div
     className={`menu-item ${size ? size : ''}`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div
       className="background-image "
@@ -13,9 +15,9 @@ const MenuItem = ({ title, imageUrl, size }) => (
     />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+    <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
