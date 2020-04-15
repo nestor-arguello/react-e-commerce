b20aade26dec6b6696060b7af7d62b9e1c21b9db
Nestor Arguello <nestor.arguello@gmail.com>
2020-04-15 18:26:24 -0400

    fix calssName prperty on back-portfolio-btn

--- src/components/back-portfolio-btn/back-portfolio-btn.component.jsx
+++ src/components/back-portfolio-btn/back-portfolio-btn.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './back-portfolio-btn.styles.css';
 
 const BackPortfolioBtn = () => (
-  <div class="back-home-btn">
+  <div className="back-home-btn">
     <a
       href="https://nestor-arguello.github.io/portfolio"
       target="_self"
