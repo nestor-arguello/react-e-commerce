5f10250885d9779d68682586547e5b4bb3f59bf0
Nestor Arguello <nestor.arguello@gmail.com>
2020-04-20 22:45:59 -0400

    Updates

---
+++ precache-manifest.c1a5598344e8e48670389420f54eeaa4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe036be6b0838a6adbc06eb6c71617e7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "38668cfd64a637994a3d",
+    "url": "/portfolio/static/css/main.ac816686.chunk.css"
+  },
+  {
+    "revision": "6230506ca6fd58881b30",
+    "url": "/portfolio/static/js/2.012f018a.chunk.js"
+  },
+  {
+    "revision": "4baf5dc16be53e517e8b1978976d5788",
+    "url": "/portfolio/static/js/2.012f018a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38668cfd64a637994a3d",
+    "url": "/portfolio/static/js/main.e0553158.chunk.js"
+  },
+  {
+    "revision": "bdeee437072178f26e6e",
+    "url": "/portfolio/static/js/runtime-main.6ac1a7a2.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/portfolio/static/media/11.2 shopping-bag.svg.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/portfolio/static/media/4.3 crown.svg.ed28accb.svg"
+  }
+]);
