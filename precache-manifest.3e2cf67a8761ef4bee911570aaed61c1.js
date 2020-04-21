6a0f3a9c769fef1899d3e83d9e9dbbf69164d354
Nestor Arguello <nestor.arguello@gmail.com>
2020-04-20 22:55:29 -0400

    Updates

---
+++ precache-manifest.3e2cf67a8761ef4bee911570aaed61c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e437ec015361838e6dde40b44c1a44f",
+    "url": "/portfolio/works/react-ecommerce/index.html"
+  },
+  {
+    "revision": "38668cfd64a637994a3d",
+    "url": "/portfolio/works/react-ecommerce/static/css/main.ac816686.chunk.css"
+  },
+  {
+    "revision": "53266c793e04f556805f",
+    "url": "/portfolio/works/react-ecommerce/static/js/2.dce25907.chunk.js"
+  },
+  {
+    "revision": "4baf5dc16be53e517e8b1978976d5788",
+    "url": "/portfolio/works/react-ecommerce/static/js/2.dce25907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38668cfd64a637994a3d",
+    "url": "/portfolio/works/react-ecommerce/static/js/main.e0553158.chunk.js"
+  },
+  {
+    "revision": "2d3f926a80b8bd5978e1",
+    "url": "/portfolio/works/react-ecommerce/static/js/runtime-main.5840a409.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/portfolio/works/react-ecommerce/static/media/11.2 shopping-bag.svg.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/portfolio/works/react-ecommerce/static/media/4.3 crown.svg.ed28accb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c1a5598344e8e48670389420f54eeaa4.js"
+  "/portfolio/works/react-ecommerce/precache-manifest.3e2cf67a8761ef4bee911570aaed61c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/works/react-ecommerce/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
